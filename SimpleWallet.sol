136c1650f2a01e161f249e8b32cd071292adba3a
unknown <rolandjpiggott@gmail.com>
2021-04-26 19:50:38 +0100

    Spacing change

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -4,9 +4,11 @@ contract SimpleWallet {
     
     function withdrawMoney(address payable _to, uint _amount) public {
         _to.transfer(_amount);
+        
     }
     
     function () external payable {
+        
         
     }
 }
